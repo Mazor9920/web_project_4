e4135635c7289d71f413b0e5f4905e588ecfd39b
Mazor Leave <mazor9920@gmail.comn>
2022-02-05 20:53:36 +0200

    refactor: refactor after code review #1

--- scripts/script.js
+++ scripts/script.js
@@ -4,37 +4,37 @@
 
 let content = document.querySelector("#content");
 // let content = document.querySelector(".content");
-
 let profile = content.querySelector(".profile");
-let profile__info = profile.querySelector(".profile__info");
-let popup = profile.querySelector(".popup");
-let popup__form_container = popup.querySelector(".popup__form-container");
-let popup__edit_form = popup__form_container.querySelector(".popup__edit-form");
+let profileInfo = profile.querySelector(".profile__info");
+
+let popup = document.querySelector(".popup");
+let popupContainer = popup.querySelector(".popup__container");
+let EditForm = popupContainer.querySelector(".popup__edit-form");
 
 // buttons
-let profile__edit_button = profile__info.querySelector(".profile__edit-button");
-let popup__close_button = popup.querySelector(".popup__close-button");
-let popup__save_button = popup__edit_form.querySelector(".popup__save-button");
+let EditButton = profileInfo.querySelector(".profile__edit-button");
+let CloseButton = popupContainer.querySelector(".popup__close-button");
+let SaveButton = EditForm.querySelector(".popup__save-button");
 
 // profile initial values
-let profile__name = profile__info.querySelector(".profile__name");
-let profile__details = profile__info.querySelector(".profile__details");
+let profileName = profileInfo.querySelector(".profile__name");
+let profileDetails = profileInfo.querySelector(".profile__details");
+
 // temp input fields
-let inputs = popup__edit_form.querySelectorAll(".popup__input-field");
-let name_input = inputs[0];
-let job_input = inputs[1];
+let nameInput = EditForm.querySelector(".popup__input-field_value_name");
+let jobInput = EditForm.querySelector(".popup__input-field_value_job");
 
 /////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////      Event Listeners      /////////////////////////
 
-profile__edit_button.addEventListener("click", changePopupStatus);
-popup__close_button.addEventListener("click", changePopupStatus);
+EditButton.addEventListener("click", changePopupStatus);
+CloseButton.addEventListener("click", changePopupStatus);
 
-profile__edit_button.addEventListener("click", loadProfile);
+EditButton.addEventListener("click", loadProfile);
 
 // watch the submit form event, it won't submit anywhere just yet
-popup__edit_form.addEventListener('submit', handleProfileFormSubmit);
+EditForm.addEventListener('submit', handleProfileFormSubmit);
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -52,16 +52,10 @@ function changePopupStatus() {
 }
 /////////////////////////////////////////////////////////////////////////////
 
-// this function show the existing profile values on input fileds, if any
+// this function showes the existing profile values on input fileds
 function loadProfile() {
-  getValues(name_input, profile__name.textContent);
-  getValues(job_input, profile__details.textContent);
-}
-
-function getValues(input_object, text_input) {
-  if (text_input !== null) {
-    input_object.value = text_input;
-  }
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDetails.textContent;
 }
 /////////////////////////////////////////////////////////////////////////////
 
@@ -71,7 +65,10 @@ function handleProfileFormSubmit(evt) {
   evt.preventDefault();
 
   // Insert new values
-  profile__name.textContent = name_input.value;
-  profile__details.textContent = job_input.value;
+  profileName.textContent = nameInput.value;
+  profileDetails.textContent = jobInput.value;
+
+  //close popup
+  changePopupStatus();
 }
 /////////////////////////////////////////////////////////////////////////////
